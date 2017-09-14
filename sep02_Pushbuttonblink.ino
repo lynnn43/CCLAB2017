7923e0b431e024f7425e1b23355dcf78f2b743ec
lynnn43 <lynnn43@users.noreply.github.com>
2017-09-14 19:16:36 -0400

    Added more LED pins
    Added more LED pins

--- sep02_Pushbuttonblink.ino
+++ sep02_Pushbuttonblink.ino
@@ -1,20 +1,5 @@
-int led = 13;
-int buttonPin = 2;
-int buttonState = 0;
-
-void setup() {
-  pinMode(led, OUTPUT);
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   buttonState = digitalRead(buttonPin);
-
-  if(buttonState == HIGH){
-    digitalWrite(led, HIGH);
-    delay(1000);
-  } else {
-    digitalWrite(led, LOW);
-    delay(1000);
-  }
-}
---
+++ sep02_Pushbuttonblink/sep02_Pushbuttonblink.ino
@@ -0,0 +1,4 @@
+int led1 = 13;
+int led2 = 12;
+int led3 = 11;
+int buttonPin = 2;
